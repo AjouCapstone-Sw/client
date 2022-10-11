50bd9486ae2d3bd4c3f6567219e47b55acc79b99
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-12 01:24:14 +0900

    fix: useMovePage Hook Handler

--- src/Hook/useMovePage.tsx
+++ src/Hook/useMovePage.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 
 export const useMovePage = (path: string | string[]) => {
   const navigate = useNavigate();
-  if (typeof path === 'string') return () => navigate(path);
+
+  if (typeof path === 'string') path = [path];
   return path.map((src) => () => navigate(src));
 };
--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -2,9 +2,11 @@ import { useLoginState } from './LoginPage.hook';
 import LoginPageStyle from './LoginPage.style';
 
 import { Button, Input } from '@Components/.';
+import { useMovePage } from '@Hook/useMovePage';
 
 export const LoginPage = () => {
   const { email, password, handleChangeEmail, handleChangePassword } = useLoginState();
+  const [goRegister] = useMovePage('/register');
   return (
     <LoginPageStyle.LoginContainer>
       <h1>로그인</h1>
@@ -39,6 +41,7 @@ export const LoginPage = () => {
           <Button
             type='button'
             radius
+            onClick={goRegister}
           >
             회원가입
           </Button>

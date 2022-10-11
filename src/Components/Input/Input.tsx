a8d815395b8a8c5ceb06613ee7921d4b10a6cd13
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-12 00:33:09 +0900

    feat: Input Component

---
+++ src/Components/Input/Input.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  background-color: #f6f6f6;
+  width: 100%;
+  height: 50px;
+  border: 1px solid #e8e8e8;
+  border-radius: 8px;
+  color: #bdbdbd;
+`;

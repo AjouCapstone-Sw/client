ebd54ddccc63c0459a77ecb8b98bfe0712f4eac8
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-12 01:21:21 +0900

    feat: LoginPage State handler

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,47 +1,55 @@
+import { useLoginState } from './LoginPage.hook';
 import LoginPageStyle from './LoginPage.style';
 
 import { Button, Input } from '@Components/.';
 
-export const LoginPage = () => (
-  <LoginPageStyle.LoginContainer>
-    <h1>로그인</h1>
-    <LoginPageStyle.LoginForm
-      style={{ width: '100%' }}
-      action='/login'
-      method='POST'
-    >
-      <LoginPageStyle.InputContainer>
-        <Input
-          placeholder='이메일'
-          type='text'
-          name='email'
-        />
-        <Input
-          placeholder='비밀번호'
-          type='password'
-          name='password'
-        />
-      </LoginPageStyle.InputContainer>
+export const LoginPage = () => {
+  const { email, password, handleChangeEmail, handleChangePassword } = useLoginState();
+  return (
+    <LoginPageStyle.LoginContainer>
+      <h1>로그인</h1>
+      <LoginPageStyle.LoginForm
+        style={{ width: '100%' }}
+        action='/login'
+        method='POST'
+      >
+        <LoginPageStyle.InputContainer>
+          <Input
+            placeholder='이메일'
+            type='text'
+            name='email'
+            value={email}
+            onChange={handleChangeEmail}
+          />
+          <Input
+            placeholder='비밀번호'
+            type='password'
+            name='password'
+            value={password}
+            onChange={handleChangePassword}
+          />
+        </LoginPageStyle.InputContainer>
 
-      <LoginPageStyle.TextContainer>
-        <LoginPageStyle.SearchText>비밀번호 찾기</LoginPageStyle.SearchText>
-        <LoginPageStyle.SearchText>이메일 찾기</LoginPageStyle.SearchText>
-      </LoginPageStyle.TextContainer>
+        <LoginPageStyle.TextContainer>
+          <LoginPageStyle.SearchText>비밀번호 찾기</LoginPageStyle.SearchText>
+          <LoginPageStyle.SearchText>이메일 찾기</LoginPageStyle.SearchText>
+        </LoginPageStyle.TextContainer>
 
-      <LoginPageStyle.AuthButtonContainer>
-        <Button
-          type='button'
-          radius
-        >
-          회원가입
-        </Button>
-        <Button
-          type='submit'
-          radius
-        >
-          로그인
-        </Button>
-      </LoginPageStyle.AuthButtonContainer>
-    </LoginPageStyle.LoginForm>
-  </LoginPageStyle.LoginContainer>
-);
+        <LoginPageStyle.AuthButtonContainer>
+          <Button
+            type='button'
+            radius
+          >
+            회원가입
+          </Button>
+          <Button
+            type='submit'
+            radius
+          >
+            로그인
+          </Button>
+        </LoginPageStyle.AuthButtonContainer>
+      </LoginPageStyle.LoginForm>
+    </LoginPageStyle.LoginContainer>
+  );
+};

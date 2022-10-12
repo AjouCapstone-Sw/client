0bce871434fea556b8f6a10e329eca63cdcc1231
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-12 13:50:31 +0900

    fix: delete dont global style https://github.com/AjouCapstone-Sw/client/pull/3\#discussion_r992594177

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import { createGlobalStyle } from 'styled-components';
-// import reset from 'styled-reset';
 
 import { worker } from './_msw';
 import App from './App';
 
-// const GlobalStyle = createGlobalStyle`${reset}`;
 worker.start();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      {/* <GlobalStyle /> */}
       <App />
     </Router>
   </React.StrictMode>,

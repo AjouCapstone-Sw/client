3c4a9d4b245b054a6472abd6eec1e96f87ff04bc
Noelsky <hansorly@ajou.ac.kr>
2022-10-16 16:02:48 +0900

    feat: RegisterPage Template

---
+++ src/Pages/RegisterPage/RegisterPage.const.ts
@@ -0,0 +1,6 @@
+const SexOption = [
+  { value: 'male', label: '남' },
+  { value: 'female', label: '여' },
+];
+
+export default { SexOption };
--- src/Pages/RegisterPage/RegisterPage.tsx
+++ src/Pages/RegisterPage/RegisterPage.tsx
@@ -1,1 +1,135 @@
-export const RegisterPage = () => <div>회원가입페이지</div>;
+import { useForm } from 'react-hook-form';
+
+import RegisterPageConstant from './RegisterPage.const';
+import { useEmailVerify } from './RegisterPage.hook';
+import RegisterPageStyle from './RegisterPage.style';
+
+import { Button, Input, Dropdown, Calendar } from '@Components/.';
+
+type RegisterFormData = {
+  id: string;
+  email: string;
+  emailVerifyNum: string;
+  password: string;
+  passwordValidate: string;
+  sex: string;
+  birth: string;
+};
+
+export const RegisterPage = () => {
+  const {
+    register,
+    handleSubmit: handleRegisterSubmit,
+    control,
+    watch,
+    formState: { errors },
+  } = useForm<RegisterFormData>();
+
+  const { emailVerifyState, handleEmailVerify } = useEmailVerify();
+
+  const onSubmit = (data: RegisterFormData) => {
+    const { id, email, password, sex, birth } = data;
+    console.log(id, email, password, sex, birth);
+  };
+
+  return (
+    <RegisterPageStyle.RegisterContainer>
+      <h1>회원 가입</h1>
+      <RegisterPageStyle.RegisterForm onSubmit={handleRegisterSubmit(onSubmit)}>
+        <div>
+          <Input
+            placeholder='아이디'
+            {...register('id', { required: '필수 응답 항목입니다.' })}
+          />
+          <RegisterPageStyle.ErrorMsg>{errors.id?.message}</RegisterPageStyle.ErrorMsg>
+          <Input
+            placeholder='비밀번호'
+            type='password'
+            {...register('password', {
+              required: '필수 응답 항목입니다.',
+              minLength: {
+                value: 8,
+                message: '비밀번호는 8자 이상이어야 합니다',
+              },
+              pattern: {
+                value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+                message: '한 개 이상의 문자와 숫자로 구성해 주세요',
+              },
+            })}
+          />
+          <RegisterPageStyle.ErrorMsg>{errors.password?.message}</RegisterPageStyle.ErrorMsg>
+          <Input
+            placeholder='비밀번호 확인'
+            type='password'
+            {...register('passwordValidate', {
+              required: '필수 응답 항목입니다.',
+              validate: (val: string) =>
+                watch('password') === val || '비밀번호가 일치하지 않습니다',
+            })}
+          />
+          <RegisterPageStyle.ErrorMsg>
+            {errors.passwordValidate?.message}
+          </RegisterPageStyle.ErrorMsg>
+          <Input
+            placeholder='이메일'
+            {...register('email', {
+              required: '필수 응답 항목입니다.',
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i,
+                message: '이메일 형식이 아닙니다.',
+              },
+            })}
+          />
+          <RegisterPageStyle.ErrorMsg>{errors.email?.message}</RegisterPageStyle.ErrorMsg>
+
+          <RegisterPageStyle.EmailVerifyContainer>
+            <Input
+              disabled={!emailVerifyState}
+              placeholder='이메일 인증번호'
+              {...register('emailVerifyNum', {
+                required: '필수 응답 항목입니다.',
+                minLength: {
+                  value: 4,
+                  message: '인증번호를 확인해 주세요',
+                },
+              })}
+            />
+            {emailVerifyState ? (
+              <Button
+                type='button'
+                onClick={handleEmailVerify}
+              >
+                인증번호 확인
+              </Button>
+            ) : (
+              <Button
+                type='button'
+                onClick={handleEmailVerify}
+              >
+                인증번호 받기
+              </Button>
+            )}
+          </RegisterPageStyle.EmailVerifyContainer>
+          <RegisterPageStyle.ErrorMsg>{errors.emailVerifyNum?.message}</RegisterPageStyle.ErrorMsg>
+
+          <RegisterPageStyle.DropdownContainer>
+            <Dropdown
+              options={RegisterPageConstant.SexOption}
+              control={control}
+              name='sex'
+              placeholder='성별'
+              rules={{ required: '필수 입력 항목입니다' }}
+            />
+            <Calendar
+              control={control}
+              label='생년월일'
+              name='birth'
+            />
+          </RegisterPageStyle.DropdownContainer>
+          <RegisterPageStyle.ErrorMsg>{errors.sex?.message}</RegisterPageStyle.ErrorMsg>
+        </div>
+        <Button>회원가입</Button>
+      </RegisterPageStyle.RegisterForm>
+    </RegisterPageStyle.RegisterContainer>
+  );
+};

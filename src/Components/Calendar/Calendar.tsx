df96f8937f2d2b50496d2691ecd6863b136f224e
Noelsky <hansorly@ajou.ac.kr>
2022-10-17 18:55:55 +0900

    enhance: CalenderChild seperate

--- src/Components/Calendar/Calendar.tsx
+++ src/Components/Calendar/Calendar.tsx
@@ -3,7 +3,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import moment from 'moment';
-import { Control, Controller, FieldValues, Path } from 'react-hook-form';
+import { Control, Controller, ControllerRenderProps, FieldValues, Path } from 'react-hook-form';
 
 type CalendarProp<T extends FieldValues> = {
   control: Control<T>;
@@ -11,21 +11,35 @@ type CalendarProp<T extends FieldValues> = {
   name: Path<T>;
 };
 
+type CalendarChildProp<T extends FieldValues> = {
+  field: ControllerRenderProps<T, Path<T>>;
+} & Pick<CalendarProp<T>, 'label'>;
+
+const CalendarChild = <T extends FieldValues>({
+  field: { value, onChange },
+  label,
+}: CalendarChildProp<T>) => (
+  <LocalizationProvider dateAdapter={AdapterMoment}>
+    <DatePicker
+      label={label}
+      onChange={(date) => {
+        onChange(moment(date).format('YYYY-MM-DD'));
+      }}
+      value={value}
+      renderInput={(params) => <TextField {...params} />}
+    />
+  </LocalizationProvider>
+);
+
 export const Calendar = <T extends FieldValues>({ control, label, name }: CalendarProp<T>) => (
   <Controller
     control={control}
     name={name}
-    render={({ field: { onChange, value } }) => (
-      <LocalizationProvider dateAdapter={AdapterMoment}>
-        <DatePicker
-          label={label}
-          onChange={(date) => {
-            onChange(moment(date).format('YYYY-MM-DD'));
-          }}
-          value={value}
-          renderInput={(params) => <TextField {...params} />}
-        />
-      </LocalizationProvider>
+    render={({ field }) => (
+      <CalendarChild
+        field={field}
+        label={label}
+      />
     )}
   />
 );

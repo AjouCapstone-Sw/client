f9e11ef55d88f73f88685107004132c074b77d98
Noelsky <hansorly@ajou.ac.kr>
2022-11-07 12:23:00 +0900

    feat: ProductDetail msw

--- src/_msw/index.ts
+++ src/_msw/index.ts
@@ -1,5 +1,6 @@
 import { setupWorker } from 'msw';
 
 import { CategoryHandler } from './Category';
+import { ProductDetailHandler } from './ProductDetail';
 
-export const worker = setupWorker(...CategoryHandler);
+export const worker = setupWorker(...CategoryHandler, ...ProductDetailHandler);

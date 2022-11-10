445339e18656caf960f302ea439e919ec08a5942
Noelsky <hansorly@ajou.ac.kr>
2022-11-10 16:59:36 +0900

    enhance: modalContext non-null assetion remove & useModal #22

--- src/Context/Modal/ModalContext.ts
+++ src/Context/Modal/ModalContext.ts
@@ -2,6 +2,6 @@ import { createContext } from 'react';
 
 import { ModalDispatch, OpenModal } from './ModalProvider.type';
 
-export const ModalStateContext = createContext<OpenModal[] | null>(null);
+export const ModalStateContext = createContext<OpenModal[]>([]);
 
-export const ModalDispatchContext = createContext<ModalDispatch | null>(null);
+export const ModalDispatchContext = createContext<ModalDispatch>({} as ModalDispatch);
--- src/Hook/useModal.ts
+++ src/Hook/useModal.ts
@@ -1,12 +1,9 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 
 import { ModalDispatchContext } from '@Context/.';
 
 export const useModal = () => {
-  const { open, close } = useContext(ModalDispatchContext)!;
-
-  const openModal = (Component: React.FC, props: any) => open(Component, props);
-  const closeModal = (Component: React.FC) => close(Component);
+  const { open: openModal, close: closeModal } = useContext(ModalDispatchContext)!;
 
   return { openModal, closeModal };
 };

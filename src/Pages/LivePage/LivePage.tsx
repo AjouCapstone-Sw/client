637ebb1d914a70408c71c32b5dd90e042865e9dc
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-10 19:16:36 +0900

    feat: LivePage call WebRTC View

--- src/Pages/LivePage/LivePage.tsx
+++ src/Pages/LivePage/LivePage.tsx
@@ -1,18 +1,16 @@
 import { useParams } from 'react-router-dom';
 
-import { Seller, Buyer } from '@Components/WebRTC';
+import { Seller, Buyer, WebRTCView } from '@Components/WebRTC';
 
 export const LivePage = () => {
   const { productId } = useParams();
   const data = localStorage.getItem('id');
-  console.log(data);
+  const Container = data === 'yj' ? Seller : Buyer;
   return (
     <div>
-      {data === 'yj' ? (
-        <Seller productId={Number(productId)}>hihi</Seller>
-      ) : (
-        <Buyer productId={Number(productId)}>hihi</Buyer>
-      )}
+      <Container productId={Number(productId)}>
+        <WebRTCView />
+      </Container>
     </div>
   );
 };

f9967d8c15c83a11adea888e53d34490e9434c4f
Noelsky <hansorly@ajou.ac.kr>
2022-11-10 18:20:54 +0900

    feat: local-https

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import * as path from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 import svgr from 'vite-plugin-svgr';
 
 const resolve = (src: string) => path.resolve(__dirname, src);
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [svgr(), react(), mkcert()],
   resolve: {
     alias: [
       { find: '@Components', replacement: resolve('src/Components') },
@@ -20,5 +21,6 @@ export default defineConfig({
   },
   server: {
     port: 3000,
+    https: true,
   },
 });

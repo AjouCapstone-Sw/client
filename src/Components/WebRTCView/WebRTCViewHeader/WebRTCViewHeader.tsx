5cb8bc568cb176a5699635dfcc3a9cfbd3890e48
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-11 16:01:58 +0900

    fix: WebRTCViewHeader Icon Const Delete && Add move Page Event

--- src/Components/WebRTCView/WebRTCViewHeader/WebRTCViewHeader.tsx
+++ src/Components/WebRTCView/WebRTCViewHeader/WebRTCViewHeader.tsx
@@ -1,19 +1,27 @@
-import { AUCTION_HEADER_IMG } from './WebRTCViewHeader.const';
 import WebRTCViewHeaderStyle from './WebRTCViewHeader.style';
 
-export const WebRTCViewHeader = () => (
-  <>
-    <div>
-      <span>경매</span>
-      <span className='strike'>Live</span>
-    </div>
-    <WebRTCViewHeaderStyle.HeaderImgContainer>
-      {AUCTION_HEADER_IMG.map(({ id, ...info }) => (
+import { useMovePage } from '@Hook/useMovePage';
+
+export const WebRTCViewHeader = () => {
+  const [goHome] = useMovePage('/') as (() => void)[];
+  return (
+    <>
+      <div>
+        <span>경매</span>
+        <span className='strike'>Live</span>
+      </div>
+      <WebRTCViewHeaderStyle.HeaderImgContainer>
         <img
-          key={id}
-          {...info}
+          src='/asset/Auction/Sound.svg'
+          alt='음소거'
         />
-      ))}
-    </WebRTCViewHeaderStyle.HeaderImgContainer>
-  </>
-);
+        <img
+          src='/asset/Auction/Exit.svg'
+          alt='나가기'
+          onClick={goHome}
+          aria-hidden
+        />
+      </WebRTCViewHeaderStyle.HeaderImgContainer>
+    </>
+  );
+};

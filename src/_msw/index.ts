fcea33518880d4f3fca1c48006725807921a0efd
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-12 20:18:15 +0900

    feat: add userInfo API MSW

--- src/_msw/index.ts
+++ src/_msw/index.ts
@@ -2,5 +2,6 @@ import { setupWorker } from 'msw';
 
 import { CategoryHandler } from './Category';
 import { ProductDetailHandler } from './ProductDetail';
+import { UserInfoHandler } from './User';
 
-export const worker = setupWorker(...CategoryHandler, ...ProductDetailHandler);
+export const worker = setupWorker(...CategoryHandler, ...ProductDetailHandler, ...UserInfoHandler);

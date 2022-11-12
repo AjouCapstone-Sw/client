73fb290ed596f82168c0ac06a8adc02ba8029fca
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-13 00:57:47 +0900

    enhance: productReview data

--- src/_msw/Review/const.ts
+++ src/_msw/Review/const.ts
@@ -1,15 +1,47 @@
 export const MSW_PRODUCT_REVIEW = [
   {
+    reviewId: 1,
     productId: 1,
-    nickName: '',
+    nickName: 'hansoly',
     review: '경매 재밌게 하시네요',
     score: 4,
     createdAt: '2022.11.08',
+    productImage: '/asset/김영진.jpg',
+  },
+  {
+    reviewId: 2,
+    productId: 2,
+    nickName: 'haasfdasdfafsdfasdsfadnsoly',
+    review:
+      '경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요',
+    score: 4,
+    createdAt: '2022.11.08',
+    productImage: '/asset/김영진.jpg',
+  },
+  {
+    reviewId: 3,
+    productId: 3,
+    nickName: 'haasfdasdfafsdfasdsfadnsoly',
+    review:
+      '경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요',
+    score: 4,
+    createdAt: '2022.11.08',
+    productImage: '/asset/김영진.jpg',
+  },
+  {
+    reviewId: 4,
+    productId: 4,
+    nickName: 'haasfdasdfafsdfasdsfadnsoly',
+    review:
+      '경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요경매 재밌게 하시네요',
+    score: 4,
+    createdAt: '2022.11.08',
+    productImage: '/asset/김영진.jpg',
   },
 ];
 export const MSW_AUCTION_REVIEW = [
   {
-    productId: 1,
+    reviewId: 1,
     nickName: '',
     review: '경매 재밌게 하시네요',
     score: 4,

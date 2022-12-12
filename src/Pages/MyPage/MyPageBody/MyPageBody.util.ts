ef2cf3146826f877ef9d26f938208f534bea9d40
홍한솔 <66724340+Noelsky-code@users.noreply.github.com>
2022-12-12 15:10:25 +0900

    Feat/admin page (#57)
    * feat: AdminPage SellList
    
    * feat: admin navigate

--- src/Pages/MyPage/MyPageBody/MyPageBody.util.ts
+++ src/Pages/MyPage/MyPageBody/MyPageBody.util.ts
@@ -3,10 +3,10 @@ import type { auctionReviewType, PointHistoryType, productReviewType } from '../
 import { AuctionReview, ItemListCell, PointHistory, ProductReview } from '@Components/.';
 import type { ItemListCellType } from '@Pages/ListPage/ListPage.type';
 
-const isItemListCell = (item: any) => (item as ItemListCellType).buyNowPrice !== undefined;
-const isProductReview = (item: any) => (item as productReviewType).productId !== undefined;
-const isAuctionReview = (item: any) => (item as auctionReviewType).score !== undefined;
-const isPointHistroy = (item: any) => (item as PointHistoryType).point !== undefined;
+const isItemListCell = (item: any) => (item as ItemListCellType)?.buyNowPrice !== undefined;
+const isProductReview = (item: any) => (item as productReviewType)?.productId !== undefined;
+const isAuctionReview = (item: any) => (item as auctionReviewType)?.score !== undefined;
+const isPointHistroy = (item: any) => (item as PointHistoryType)?.point !== undefined;
 
 export const getMyPageBodyChildren = (item: any) => {
   if (isItemListCell(item)) return ItemListCell;
